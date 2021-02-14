f0ac256a763bb9c3e36811e5209736499d37020b
juliastodolya <y.stodolya@gmail.com>
2021-02-14 19:01:40 +0300

    alertSheet

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(_ sender: Any) {
+        
+        
         let alert = UIAlertController(title: "ALERT!", message: "message!", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         
@@ -30,7 +32,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showActionSheet(_ sender: Any) {
+        enum Films: String {
+            case filmOne = "Film 1"
+            case filmTwo = "Film 2"
+        }
+        //хендлер возвращает экшн в зависимости от типа, который в него передали
+        let handler: (_ type: Films) -> (UIAlertAction) -> Void =
+            { type in
+                return { action in
+                    print(type.rawValue)
+                }
+        }
         
+        let alert = UIAlertController(title: "ALERT!", message: "message!", preferredStyle: .actionSheet)
+        let okAction = UIAlertAction(title: Films.filmOne.rawValue, style: .default, handler: handler(.filmOne))
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
         
     }
 }
