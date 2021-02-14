e48937f1106349c2f3c47e81a2a7e39ce99f2f9a
juliastodolya <y.stodolya@gmail.com>
2021-02-14 15:46:47 +0300

    added project

---
+++ Alert/Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Юлия on 14.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
